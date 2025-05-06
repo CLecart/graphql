aa3cb2ee6424e113358325255350be159a4fd423
clecart <djlike@hotmail.fr>
2025-05-06 12:40:55 +0200

    svg login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,13 @@
 import LoginForm from "@/components/login/login";
+import ThemeToggle from "@/components/ui/ThemeToggle";
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <>
+      <div className="absolute top-4 right-4 z-20">
+        <ThemeToggle />
+      </div>
+      <LoginForm />
+    </>
+  );
 }
